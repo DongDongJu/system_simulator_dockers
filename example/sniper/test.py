e314e84db2a06e9d2816855aa19d575875e8b4de
DongDongJu <commisori28@gmail.com>
2020-03-19 02:20:32 +0900

    sniper example

--- example/sniper/test.py
+++
@@ -1,5 +0,0 @@
-result  = 1
-for x in range(40):
-    result *= 2
-
-print(result)
---
+++ sniper/sniper/pre_mobilenet.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+./test_dcgan.py
+./test_resnet18.py
+./test_shufflenet_v2.py
+./test_mobilenet_v2.py
+./test_resnet34.py
+./test_squeezenet.py
+./test_resnet101.py
+./test_roberta.py
+./test_unet_for_brain_MRI.py
